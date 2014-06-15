db9d99ecc6bda5248595233ee808cad2d520b1c4
Sean Killeen <SeanKilleen@gmail.com>
2014-06-15 17:08:35 -0400

    Test to verify algorithm
    In case they FogCreek was being extra sneaky.

---
+++ src/test/FCApp.Core.Tests/SuppliedHashFunctionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCApp.Core.Tests
+{
+    using FluentAssertions;
+
+    using Xunit;
+
+    public class SuppliedHashFunctionTests
+    {
+        [Fact]
+        public void TheyDidntLie_ItReturnsTheExpectedResult()
+        {
+            SuppliedHashFunction.Hash("leepadg").Should().Be(680131659347);
+        }
+    }
+}
