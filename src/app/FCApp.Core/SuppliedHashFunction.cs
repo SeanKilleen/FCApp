1f5920f418846b4ab756b1f00f7cf0fd63000623
Sean Killeen <SeanKilleen@gmail.com>
2014-06-15 17:06:36 -0400

    Initial algorithm copied.

---
+++ src/app/FCApp.Core/SuppliedHashFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCApp.Core
+{
+    public static class SuppliedHashFunction
+    {
+        public static long Hash(string s)
+        {
+            long h = 7;
+            string letters = "acdegilmnoprstuw";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                h = (h * 37 + letters.IndexOf(s[i]));
+            }
+            return h;
+        }
+    }
+}
