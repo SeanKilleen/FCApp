31b34b35d7a8863178529a51baf9640e49fb0ee6
Sean Killeen <SeanKilleen@gmail.com>
2014-06-15 17:19:39 -0400

    Initial failing test & revealer class

---
+++ src/app/FCApp.Core/HashStringRevealer.cs
@@ -0,0 +1,10 @@
+﻿namespace FCApp.Core
+{
+    public class HashStringRevealer
+    {
+        public static string RevealHashString(long hashResultToReveal)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/test/FCApp.Core.Tests/HashStringRevealerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCApp.Core.Tests
+{
+    using FluentAssertions;
+
+    using Xunit;
+
+    public class HashStringRevealerTests
+    {
+        [Fact]
+        public void GivenInitialFCExample_ProducesSameResult()
+        {
+            HashStringRevealer.RevealHashString(680131659347).Should().Be("leepadg");
+        }
+    }
+}
